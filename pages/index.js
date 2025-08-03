0dfec0dd34c48aa0c1a7faa7d260d7f958354488
dhenynteam-wq <dhenynteam@gmail.com>
2025-08-03 23:08:26 +0700

    Add index.js homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 50, fontFamily: 'Arial', textAlign: 'center' }}>
+      <h1>✅ AGC Simulasi 303</h1>
+      <p>Contoh halaman utama berhasil jalan di Vercel.</p>
+    </div>
+  )
+}
