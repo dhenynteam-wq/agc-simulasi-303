95c7a6735c87b07d915bee17c6618c8842ee75a8
dhenynteam-wq <dhenynteam@gmail.com>
2025-08-03 23:22:16 +0700

    Update api-scraper.js

--- pages/video/api-scraper.js
+++ pages/video/api-scraper.js
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <h1>API Scraper Judol</h1>
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>✅ AGC Simulasi 303</h1>
+      <p>Halaman API Scraper Judol</p>
+    </div>
+  );
 }
