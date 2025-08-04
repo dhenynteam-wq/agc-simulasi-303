d554b1af8aa30acb144a565d14730a51bef76f82
dhenynteam-wq <dhenynteam@gmail.com>
2025-08-04 08:23:37 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,9 @@ import AGCLayout from '../components/agc_layout';
 export default function Home() {
   return (
     <AGCLayout>
-      <p>Contoh halaman utama berhasil jalan di Vercel.</p>
-      <ul>
-        <li><a href="/video/jaringan-qq">Jaringan QQ</a></li>
-        <li><a href="/video/dpwd-crypto">Deposit/Withdraw Crypto</a></li>
-        <li><a href="/video/simulasi-masuk">Simulasi Masuk Bot</a></li>
-        <li><a href="/video/api-scraper">API Scraper</a></li>
-      </ul>
+      <p>Selamat datang di simulasi. Pilih topik video di menu atas.</p>
     </AGCLayout>
   );
 }
+
 
